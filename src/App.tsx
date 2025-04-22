641cf88a6e9291ead840d0aa9772755ed2f96249
bryan lin <920616bryanlin@gmail.com>
2025-04-22 16:59:59 +0800

    change websockUrl to specific ip

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="app">
       <h1>協同繪圖示範</h1>
       <p>此畫布與所有訪問相同房間的用戶同步。您可以一起創作！</p>
-      <CollaborativeCanvas roomName="canvas-demo" websocketUrl="ws://localhost:1234" />
+      <CollaborativeCanvas roomName="canvas-demo" websocketUrl="ws://192.168.1.135:1234" />
     </div>
   );
 }
